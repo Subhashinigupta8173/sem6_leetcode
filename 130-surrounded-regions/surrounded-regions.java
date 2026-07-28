64ae1f36a1054433d8afde28795d78f60dbb74d3
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-28 20:22:23 +0530

    Time: 2 ms (72.90%) | Memory: 47.7 MB (75.70%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,90 +1,68 @@
-import java.util.*;
-
 class Solution {
-    public void solve(char[][] board) {
 
-        Queue<int[]> q = new LinkedList<>();
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
+
+    public void solve(char[][] board) {
 
         int n = board.length;
         int m = board[0].length;
 
         // First row and Last row
-        for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
 
-            // First row
-            if (board[0][i] == 'O') {
-                q.offer(new int[]{0, i});
-                board[0][i] = '#';
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
             }
 
-            // Last row
-            if (board[n - 1][i] == 'O') {
-                q.offer(new int[]{n - 1, i});
-                board[n - 1][i] = '#';
+            if (board[n - 1][j] == 'O') {
+                dfs(board, n - 1, j);
             }
         }
 
         // First column and Last column
         for (int i = 0; i < n; i++) {
 
-            // First column
             if (board[i][0] == 'O') {
-                q.offer(new int[]{i, 0});
-                board[i][0] = '#';
+                dfs(board, i, 0);
             }
 
-            // Last column
             if (board[i][m - 1] == 'O') {
-                q.offer(new int[]{i, m - 1});
-                board[i][m - 1] = '#';
+                dfs(board, i, m - 1);
             }
         }
 
-        // Directions
-        int[] dr = {0, 0, -1, 1};
-        int[] dc = {-1, 1, 0, 0};
-
-        // BFS
-        while (!q.isEmpty()) {
-
-            int[] curr = q.poll();
-
-            int r = curr[0];
-            int c = curr[1];
-
-            for (int i = 0; i < 4; i++) {
-
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n &&
-                    nc >= 0 && nc < m &&
-                    board[nr][nc] == 'O') {
-
-                    // Safe mark
-                    board[nr][nc] = '#';
-
-                    // Queue me daal do
-                    q.offer(new int[]{nr, nc});
-                }
-            }
-        }
-
-        // Final traversal
+        // Convert board
         for (int i = 0; i < n; i++) {
-
             for (int j = 0; j < m; j++) {
 
-                // Surrounded O -> X
                 if (board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
 
-                // Safe # -> O
                 else if (board[i][j] == '#') {
                     board[i][j] = 'O';
                 }
             }
+        }
+    }
+
+    public void dfs(char[][] board, int r, int c) {
+
+        int n = board.length;
+        int m = board[0].length;
+
+        // Base case
+        if (r < 0 || r >= n || c < 0 || c >= m || board[r][c] != 'O') {
+            return;
+        }
+
+        // Mark as safe
+        board[r][c] = '#';
+
+        // Visit 4 directions
+        for (int i = 0; i < 4; i++) {
+            dfs(board, r + dr[i], c + dc[i]);
         }
     }
 }
