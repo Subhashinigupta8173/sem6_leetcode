b958948ac642a134ddd62320abaf89e684d4ca56
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-31 19:26:42 +0530

    Time: 1 ms (100.00%) | Memory: 48.2 MB (52.93%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int [] freq = new int[26];
+        for(char ch:tasks){
+            freq[ch-'A']++;
+        }
+        int maxfreq = 0;
+        for(int f:freq){
+            maxfreq = Math.max(maxfreq, f);
+        }
+        int maxcount = 0;
+        for(int f :freq){
+            if(f == maxfreq){
+                maxcount++;
+            }
+        }
+        int ans = (maxfreq-1)*(n+1)+maxcount;
+        return  Math.max(ans, tasks.length);
+
+    }
+}
