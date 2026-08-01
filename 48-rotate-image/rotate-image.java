3f1ff87d67b8bc3ea74fb245e86b230a990c1e58
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-01 22:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (35.04%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,30 +1,56 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+        int n = matrix.length;
+        
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-         
-        for(int i = 0; i < n; i++){
 
-            int left = 0;
-            int right = n - 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
 
-            while(left < right){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
 
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
+        
+        int n = scanner.nextInt();
 
-                left++;
-                right--;
+        if (n <= 0) {
+            System.out.println();
+            return;
+        }
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
             }
         }
 
-        
+        Solution solution = new Solution();
+        solution.rotate(matrix);
+
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
