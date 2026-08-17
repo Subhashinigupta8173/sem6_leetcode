6dac0090e7ac6d2790c46317d06f29e6f4f71138
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-17 10:43:08 +0530

    Time: 15 ms (38.98%) | Memory: 56.6 MB (50.58%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int change(int sum, int[] coins) {
+        int n  = coins.length;
+        int[][] dp = new int[n + 1][sum + 1];
+        
+       
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if( i  == 0){
+                    dp[i][j] = 0;
+                }
+                else if( j == 0){
+                     dp[i][j] = 1;
+                } 
+                
+               
+                // Take the coin if possible
+                else if (coins[i - 1] <= j) {
+
+                    dp[i][j] = 
+                            dp[i-1][j]
+                             + dp[i][j - coins[i - 1]];
+                
+                }
+                else {
+                     dp[i][j] = dp[i-1][j];
+                } 
+
+            }
+        }
+        return dp[n][sum];
+        
+    }
+}
