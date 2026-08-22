8503c2e65d53b8243b73a2c9bf2893489a08d022
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-23 00:07:03 +0530

    Time: 315 ms (5.99%) | Memory: 43.2 MB (71.75%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.java
+++ 473-matchsticks-to-square/matchsticks-to-square.java
@@ -2,7 +2,6 @@ class Solution {
     List<Integer> ll;
 
     public boolean makesquare(int[] matchsticks) {
-
         int sum = 0;
 
         ll = new ArrayList<>();
@@ -23,28 +22,28 @@ class Solution {
         return solve(0, 0, 0, 0, 0, tar);
     }
 
-    public boolean solve(int s1, int s2, int s3, int s4,
-                         int i, int tar) {
+    public boolean solve(int s1, int s2, int s3, int s4, int i, int tar) {
 
-       
-        if (s1 > tar || s2 > tar ||
-            s3 > tar || s4 > tar) {
+        if (s1 > tar || s2 > tar || s3 > tar || s4 > tar) {
             return false;
         }
 
-        
         if (i == ll.size()) {
-            return s1 == tar &&
-                   s2 == tar &&
-                   s3 == tar &&
-                   s4 == tar;
+            if (s1 == tar && s2 == tar && s3 == tar && s4 == tar) {
+                return true;
+            }
+            return false;
         }
 
-        int stick = ll.get(i);
+        if (
+            solve(s1 + ll.get(i), s2, s3, s4, i + 1, tar) ||
+            solve(s1, s2 + ll.get(i), s3, s4, i + 1, tar) ||
+            solve(s1, s2, s3 + ll.get(i), s4, i + 1, tar) ||
+            solve(s1, s2, s3, s4 + ll.get(i), i + 1, tar)
+        ) {
+            return true;
+        }
 
-        return solve(s1 + stick, s2, s3, s4, i + 1, tar) ||
-               solve(s1, s2 + stick, s3, s4, i + 1, tar) ||
-               solve(s1, s2, s3 + stick, s4, i + 1, tar) ||
-               solve(s1, s2, s3, s4 + stick, i + 1, tar);
+        return false;
     }
 }
