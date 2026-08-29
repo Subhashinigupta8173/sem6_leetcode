b052bb465bd571d7cd079ac779ff491758480b60
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-29 19:09:37 +0530

    Time: 1 ms (39.33%) | Memory: 46.7 MB (61.91%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,35 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        int originalColor = image[sr][sc];
-         if (originalColor == color)
-            return image;
+    int n = image.length;
+    int m = image[0].length;
+    
 
-        Queue<int []>q = new LinkedList<>();
-        
-        q.offer(new int []{sr,sc});
-        image[sr][sc] = color;
-        int [] dr = {-1,1,0,0};
-        int [] dc=  {0,0,-1,1};
-        while(!q.isEmpty()){
-
-            int[] curr =  q.poll();
-            int r = curr[0];
-            int c = curr[1];
-            for(int i = 0; i < 4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == originalColor ){
-                    image[nr][nc] = color;
-                    q.offer(new int []{nr,nc});
-                }
-            }
-
-        }
+    Queue<int[]> q = new LinkedList<>();
+    q.offer(new int[]{sr,sc});
+    int pixel =image[sr][sc];
+    image[sr][sc] = color;
+    int [] dr = {-1,1,0,0};
+    int [] dc = {0,0,-1,1};
+    if(pixel == color){
         return image;
+    }
+    while(!q.isEmpty()){
+        int [] curr = q.poll();
+        int r = curr[0];
+        int c = curr[1];
+        for(int i = 0; i<4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(nc >= 0 && nc< m && nr >=0 && nr < n && image[nr][nc] == pixel ){
+                q.offer(new int[]{nr,nc});
+                image[nr][nc] = color;
+
+            }
+        }
+    }
+    return image;
+
+
+        
     }
 }
