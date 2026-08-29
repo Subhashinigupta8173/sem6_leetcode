a4173f0a4afe1b0ab272b78dd2c1c4bde1af562f
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-29 14:39:36 +0530

    Time: 15 ms (64.29%) | Memory: 47.9 MB (33.02%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,49 +1,35 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-
         int n = grid.length;
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
             return -1;
-
+        }
         Queue<int[]> q = new LinkedList<>();
-
-        q.offer(new int[]{0, 0, 1});
-
+        q.offer(new int[]{0,0,1});
         grid[0][0] = 1;
-
         int[] dr = {-1,-1,-1,0,0,1,1,1};
         int[] dc = {-1,0,1,-1,1,-1,0,1};
 
-        while (!q.isEmpty()) {
-
-            int[] curr = q.poll();
-
-            int row = curr[0];
-            int col = curr[1];
-            int dist = curr[2];
-
-            if (row == n - 1 && col == n - 1)
+        while(!q.isEmpty()){
+            int [] top = q.poll();
+            int r = top[0];
+            int c = top[1];
+            int dist = top[2];
+            if(r == n -1 && c == n - 1){
                 return dist;
 
-            for (int k = 0; k < 8; k++) {
-
-                int newRow = row + dr[k];
-                int newCol = col + dc[k];
-
-                if (newRow >= 0 &&
-                    newRow < n &&
-                    newCol >= 0 &&
-                    newCol < n &&
-                    grid[newRow][newCol] == 0) {
-
-                    grid[newRow][newCol] = 1;
-
-                    q.offer(new int[]{newRow, newCol, dist + 1});
+            }
+            for(int k = 0; k < 8;k++){
+                int newrow = r + dr[k];
+                int newcol = c + dc[k];
+                if(newrow >= 0 && newrow < n  && newcol >= 0 && newcol < n && grid[newrow][newcol] == 0 ){
+                    grid[newrow][newcol] = 1;
+                    q.offer(new int []{newrow,newcol,dist+1});
                 }
             }
-        }
 
+        }
         return -1;
+        
     }
 }
