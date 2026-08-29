d25ae28924df9d601f3c55b69b46e2931338a438
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-29 19:44:44 +0530

    Time: 7 ms (14.26%) | Memory: 51.7 MB (80.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,40 @@
 class Solution {
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
+    boolean [][] visited;
     public int numIslands(char[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
-        int islands = 0;
-        Queue<int []> q = new LinkedList<>();
-        for(int i = 0; i < m ; i++){
-            for(int j = 0;j < n;j++){
+        int count = 0 ;
+        int[] dc = {-1, 1, 0, 0};
+        int[] dr = {-0 ,0, -1, 1};
+        Queue<int[]>q = new LinkedList<>();
+        for(int i  =0;i < m;i++){
+            for(int j = 0; j < n;j++){
                 if(grid[i][j] == '1'){
-                    islands++;
-                    grid[i][j] = '0';
-                    q.offer(new int []{i,j});
+                    grid[i][j] = 'A';
+                    count++;
+                    q.offer(new  int []{i,j});
                     while(!q.isEmpty()){
-                        int [] curr = q.poll();
-                        int r = curr[0];
-                        int c = curr[1];
-                        for(int k = 0 ; k < 4 ; k++){
+                        int [] top = q.poll();
+                        int r = top[0];
+                        int c = top[1];
+                        for(int k  = 0;k<4;k++){
                             int nr = r + dr[k];
                             int nc = c + dc[k];
-                            if(nc >= 0 && nc < n && nr >= 0 && nr < m && grid[nr][nc] ==  '1'){
-                                grid[nr][nc] = '0';
+                            if(nr>= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == '1'){
                                 q.offer(new int[]{nr,nc});
+                                grid[nr][nc] = 'A';
                             }
                         }
-
                     }
-                }
 
+                }
             }
         }
-        return islands;
+        return count;
 
 
-        
     }
+    
+
 }
