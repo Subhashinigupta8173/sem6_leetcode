7d46d0534b9cd66ff44c4baad1110eb84beb9df3
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-30 10:40:00 +0530

    Time: 2 ms (90.52%) | Memory: 48.1 MB (14.12%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,26 +1,22 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
+    List<List<Integer>>  ans = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-
-        List<Integer> ways = new LinkedList<>();
+        List<Integer>  ways = new ArrayList<>();
         ways.add(0);
-        dfs(0, graph, ways);
-
+        dfs(0,graph,ways);
         return ans;
-
+    
         
     }
-    private void dfs(int node,int[][] graph,List<Integer> ways){
-        if(node == graph.length - 1){
+    public  void dfs(int i,int [][] graph,List<Integer> ways){
+        if(i == graph.length-1){
             ans.add(new ArrayList<>(ways));
             return ;
         }
-        for(int nbrs : graph[node]){
+        for(int nbrs : graph[i]){
             ways.add(nbrs);
             dfs(nbrs,graph,ways);
             ways.remove(ways.size()-1);
-
         }
     }
-
 }
