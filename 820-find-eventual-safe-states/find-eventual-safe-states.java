9ea1969545e98c0c9da2ff9cce44747e3c25c480
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-30 11:07:16 +0530

    Time: 24 ms (44.66%) | Memory: 61.3 MB (67.88%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,40 +1,40 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        List<Integer>[] rev = new ArrayList[n];
-        for(int i = 0; i<n;i++){
-            rev[i]=new ArrayList<>();
+        ArrayList<Integer>[] reverse = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            reverse[i] = new ArrayList<>();
         }
-        int [] outdegree = new int [n];
-        for(int i =0; i<n;i++){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] outdegree = new int[n];
+        for (int i = 0; i < n; i++) {
             outdegree[i] = graph[i].length;
-            for(int nbrs : graph[i]){
-                rev[nbrs].add(i) ;//reverse edge create kr rha haiye,rev[1].add(0)
+            for (int nbrs : graph[i]) {
+                reverse[nbrs].add(i);
             }
-        }
-        Queue<Integer>q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(outdegree[i] == 0){
-                q.offer(i);
-            }
-        }
 
-        List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()){
-            int node = q.poll();//outdegree 0 vale phle se hi node me hai jo ki termnial hai 
-            ans.add(node);//isliye ans me directadd kar diya
-            for(int parent: rev[node]){
-                outdegree[parent]--;
-                if(outdegree[parent] == 0){
-                    q.offer(parent);
+        }
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (outdegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            ans.add(curr);
+            for (int node : reverse[curr]) {
+                outdegree[node]--;
+                if (outdegree[node] == 0) {
+                    q.add(node);
+
                 }
             }
+
         }
         Collections.sort(ans);
         return ans;
 
-         
-
-        
     }
 }
